942481336cff5ecb76fc01d4c5651921581a8b5b
nicogutich19 <nicogutich19@gmail.com>
2025-10-20 21:53:43 -0500

    Proceso de animación

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,16 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        playerInput = GetCoponent<PlayerInput>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        playerRb.LinearVelocity = new Vector2(moveInput.x * SpeedTreeWindAsset, playerRb
     }
 }
