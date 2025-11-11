1aa2a99dcedf1c7070ceb1474327a74236262a78
nicogutich19 <nicogutich19@gmail.com>
2025-11-10 21:34:59 -0500

    Bala Player 1.0

---
+++ Assets/Scripts/Bullet Script.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Scripts/Enemie 1 Mov.cs
+++ Assets/Scripts/Enemie 1 Mov.cs
@@ -7,13 +7,13 @@ public class Enemie1 : MonoBehaviour
     public float limiteIzquierdo = -15f;
 
     private bool movimientoDerecha = true;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (movimientoDerecha)
