8a4e2f43f74e80cb0dd896a9ef602feefd35453f
nicogutich19 <nicogutich19@gmail.com>
2025-11-14 15:47:45 -0500

    Instancia Bala V1

---
+++ Assets/Scripts/Bullet FPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerBulletFire : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (bulletPrefab != null && firePoint != null)
+            {
+                Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEditorInternal;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+public class playerControler : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Movimiento horizontal
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new UnityEngine.Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        // Verificar si está en el suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        // Saltar
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+    }
+}
